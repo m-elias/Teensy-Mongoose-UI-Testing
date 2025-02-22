4d0f8507ec6dc12fca98e888dda34a23e69860d6
m-elias <eliaschief@gmail.com>
2025-02-22 17:30:53 -0600

    Update LEDS.h

--- Teensy_UI/LEDS.h
+++ Teensy_UI/LEDS.h
@@ -33,6 +33,7 @@ typedef enum {
   STAGE2_RED_BLINK,
   STAGE3_GREEN_BLINK,
   STAGE4_GREEN,
+  STAGE5_AMBER,
 } LED_STAGE;
 
 typedef enum {
@@ -118,9 +119,18 @@ public:
 
   void init() {
     pinMode(GGA_LED, OUTPUT);
+
     ledDriver.begin();
+
+    Serial.print("\r\nInitializing RGB LED Outputs");
+    Wire.beginTransmission(0x70);
+    Serial.print("\r\n- RGB PCA9685 ");
+    if (Wire.endTransmission() == 0)
+      Serial.print("found");
+    else
+      Serial.print("*NOT found!*");
+
     Wire.setClock(1000000);
-    //ledDriver.setOscillatorFrequency(27000000);
     ledDriver.setPWMFreq(120); // 120 hz should be enough to not notice, but increase if flickering
     ledDriver.setOutputMode(false); // false: open drain, true: totempole (push/pull)
     allLedOff();
@@ -154,6 +164,10 @@ public:
       case 8: // 8: Simulation mode
         set(LED_ID::GPS, STAGE4_GREEN, _debug);
         return;
+      case 9:
+        set(LED_ID::GPS, STAGE5_AMBER, _debug); // UDP pass through
+        blueFlashEnabled = false;
+        return;
       case 3:
       case 7:
       default: // 3: Not applicable, 7: Manual input mode
@@ -174,6 +188,9 @@ public:
     } else if (_stage == STAGE1_RED || _stage == STAGE2_RED_BLINK) {
       data[_id].redValue = redBrightnessScale;
       data[_id].greenValue = 0;
+    } else if (_stage == STAGE5_AMBER){
+      data[_id].redValue = redBrightnessScale * 0.75;
+      data[_id].greenValue = greenBrightnessScale * 0.8;
     } else { // any other value, set OFF (stage: 0)
       _stage = 0;
       data[_id].redValue = 0;
